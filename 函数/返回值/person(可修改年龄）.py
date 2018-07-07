cc9cd84467d63d71546c0d64e74944749259e2ea
ReWKing <baymaxing@gmail.com>
2018-07-07 22:33:13 +0800

    第三次通过pycharm提交代码到github

--- 函数/返回值/person(可修改年龄）.py
+++ 函数/返回值/person(可修改年龄）.py
@@ -1,7 +1,7 @@
 # author：K
 def build_person(first_name, last_name, age=''):
     """返回一个字典，其中包含有关一个人的信息"""
-    person = {'first': first_name, 'last':last_name}
+    person = {'first': first_name, 'last': last_name}
     if age:
         person['age'] = age
         return person
