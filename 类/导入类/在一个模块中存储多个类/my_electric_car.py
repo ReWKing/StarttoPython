87deef69bef2565d066b6281109c19eb1f27118d
ReWKing <baymaxing@gmail.com>
2018-07-13 16:40:26 +0800

    Update

--- 类/导入类/在一个模块中存储多个类/my_electric_car.py
+++ 类/导入类/在一个模块中存储多个类/my_electric_car.py
@@ -1,8 +1,8 @@
 # author：K
-from car import Car, ElectricCar
+from car import ElectricCar
 
-my_beetle = Car('volkswagen', 'beetle', '2016')
-print(my_beetle.get_descriptive_name())
+my_tesla = ElectricCar('tesla', 'model s', 2016)
 
-my_tesla = ElectricCar('tesla', 'roadster', 2016)
 print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
