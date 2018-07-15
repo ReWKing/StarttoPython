2d073ae8113d60d4a792ec688a3cefdd6abfde89
ReWKing <baymaxing@gmail.com>
2018-07-15 15:56:58 +0800

    Update again

--- 测试代码/测试函数/name_function.py
+++ 测试代码/测试函数/name_function.py
@@ -1,5 +1,6 @@
 # author：K
 def get_formatted_name(first, last):
     """Generate a nearly formatted full name"""
+    # 接受名和姓并返回整洁的姓名
     full_name = first + ' ' + last
     return full_name.title()
